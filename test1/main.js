03ba40969c68317ddd292d39fd39cb6d0f74d83e
Igor <igorkutilin30@gmail.com>
2020-05-14 16:24:51 +0300

    original 12 work

--- test1/main.js
+++ test1/main.js
@@ -1,11 +1,11 @@
-let conFirm = [];
+// let conFirm = [];
 
-conFirm[0] = prompt('Как ваше Имя?', 'Имя');
-conFirm[1] = +prompt('Сколько вам лет?', 'Возрост');
-conFirm[2] = prompt('Ваш город', 'Город');
-conFirm[3] = +prompt('Ваш номер телефона', 'Номер');
+// conFirm[0] = prompt('Как ваше Имя?', 'Имя');
+// conFirm[1] = +prompt('Сколько вам лет?', 'Возрост');
+// conFirm[2] = prompt('Ваш город', 'Город');
+// conFirm[3] = +prompt('Ваш номер телефона', 'Номер');
 
-console.log(conFirm);
+// console.log(conFirm);
 
 
 
